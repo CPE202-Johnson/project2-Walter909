9b1a2974193a6187e5babb7cd51655ad34abd8a3
walter <wtoriola@calpoly.edu>
2021-02-02 09:55:20 -0800

    -Still working on testcases

--- exp_eval_tests.py
+++ exp_eval_tests.py
@@ -4,29 +4,44 @@ import unittest
 from exp_eval import *
 
 class test_expressions(unittest.TestCase):
-    def test_postfix_eval_01(self):
-        self.assertAlmostEqual(postfix_eval("3 5 +"), 8)
-
-    def test_postfix_eval_02(self):
+    def test_postfix_eval_invalid_token(self):
         try:
             postfix_eval("blah")
             self.fail()
         except PostfixFormatException as e:
             self.assertEqual(str(e), "Invalid token")
+        with self.assertRaises(PostfixFormatException):
+            postfix_eval('3 4 e')
 
-    def test_postfix_eval_03(self):
+    def test_postfix_eval_insufficient_operands(self):
         try:
             postfix_eval("4 +")
             self.fail()
         except PostfixFormatException as e:
             self.assertEqual(str(e), "Insufficient operands")
+        with self.assertRaises(PostfixFormatException):
+            postfix_eval('3 4 + 2 / +')
 
-    def test_postfix_eval_04(self):
+    def test_postfix_eval_too_many_operands(self):
         try:
             postfix_eval("1 2 3 +")
             self.fail()
         except PostfixFormatException as e:
             self.assertEqual(str(e), "Too many operands")
+        with self.assertRaises(PostfixFormatException):
+            postfix_eval('3 4 + 2')
+
+    def test_postfix_eval_invalid_bitshift(self):
+        with self.assertRaises(PostfixFormatException):
+            postfix_eval('3 1.0 <<')
+        with self.assertRaises(PostfixFormatException):
+            postfix_eval('3 1.0 >>')
+
+    def test_postfix_eval_divide_by_zero(self):
+        with self.assertRaises(ValueError):
+            postfix_eval('3 0 /')
+        with self.assertRaises(ValueError):
+            postfix_eval('3 3 - 0 /')
 
     def test_postfix_eval_simple(self):
         self.assertEqual(postfix_eval("3 5 +"), 8)
@@ -46,8 +61,6 @@ class test_expressions(unittest.TestCase):
         self.assertEqual(postfix_eval("-3 3 **"), -27)
         self.assertEqual(postfix_eval('-2 1 <<'), -4)
         self.assertEqual(postfix_eval('-2 1 >>'), -1)
-
-
 
 if __name__ == "__main__":
     unittest.main()
